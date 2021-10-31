5e8d5ad5d7e7e2dd71b9695f6ef6a3e462a8a0b8
Michal Jablonski <michal.ttl@gmail.com>
2021-10-31 10:47:28 +0100

    Improve comments.

--- primes_to_sound.py
+++ primes_to_sound.py
@@ -31,14 +31,16 @@ def is_prime(n: int) -> bool:
 
     return True
 
+# If you want to change / optimize the is_prime() function you may want to uncomment bellow function to allow pytest
+# to check that it continues to work ok.
 
-def dont_test_is_prime():
-    prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
-
-    for val in range(max(prime_list) + 1):
-        ans = is_prime(val)
-        print(f'for value {val} ans is {ans}')
-        assert ans is True if val in prime_list else ans is False
+# def test_is_prime():
+#     prime_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71]
+#
+#     for val in range(max(prime_list) + 1):
+#         ans = is_prime(val)
+#         print(f'for value {val} ans is {ans}')
+#         assert ans is True if val in prime_list else ans is False
 
 
 def show_plot():  # optional
@@ -60,11 +62,11 @@ def show_plot():  # optional
 
 if __name__ == "__main__":
 
-    # Convert prime number positions to an audio waveform using pulse density approach.
+    # Convert prime number positions to an audio waveform using pulse density, digital to analog conversion.
 
     # Define how many values will be checked if they belong to primes.
     SEARCH_LEN = int(500000)
-    # Width of the addition window for the pulse density conversion of prime positions into audio signal.
+    # Width of the addition window for the pulse density conversion.
     DAC_SIGMA_FRAME_LEN = 256
     # Below value in Hz should correspond to something your sound card can support. (44.1kHz is usually default)
     AUDIO_OUTPUT_SAMPLING_FREQUENCY = 44100
@@ -80,7 +82,7 @@ if __name__ == "__main__":
 
     print(f'Found {np.sum(primes_positions):.0f} primes up to {SEARCH_LEN}')
 
-    # generate a signal using pulse density conversion:
+    # generate a waveform using pulse density conversion:
     print(f'\nGenerating pulse density waveform at:\n'
           f'    DAC_SIGMA_FRAME_LEN = {DAC_SIGMA_FRAME_LEN}')
     for x in range(SEARCH_LEN-DAC_SIGMA_FRAME_LEN):
